0fcd34db17b4be8795b220d1256cc61d085151ae
Luke Swart <lukeswart@gmail.com>
2015-09-08 23:22:10 -0700

    Add web-mode configs

--- modes.el
+++ modes.el
@@ -205,6 +205,27 @@
               'nil)
             (local-set-key (kbd "C-c o") 'markdown-export-and-preview)))
 
+;; WEB MODE
+;; http://web-mode.org/
+;; (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  ;; (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-current-element-highlight t)
+  (lambda ()
+    (linum-mode))
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 ;; ORG MODE
 (setq org-log-done t)
 (define-key global-map "\C-cl" 'org-store-link)
