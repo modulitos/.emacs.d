869476743b6d3c6ab5b777e404238d88b300d7b8
Luke Swart <lukeswart@gmail.com>
2016-01-19 13:17:16 -0800

    Add clip file function

--- functions.el
+++ functions.el
@@ -6,6 +6,37 @@
     (delete-region (region-beginning) (region-end))
     (insert reversed)))
 
+;; These next two functions copy the filename to the os and emacs clipboards
+;; http://stackoverflow.com/questions/18812938/copy-full-file-path-into-copy-paste-clipboard
+(defun clip-file ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      (file-name-directory default-directory)
+                    (buffer-file-name))))
+    (when filename
+      (x-select-text filename))))
+
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copy the buffer-file-name to the kill-ring"
+  (interactive "cCopy Buffer Name (F) Full, (D) Directory, (N) Name")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                  (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?d)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
+
+
+
 ;; CUSTOM HIGHLIGHTING
 (defun testing-MapAppLog.txt ()
   "Toggle highlighting `TestQueryLogic' or `invoking fork-join' and `testGudermann'."
