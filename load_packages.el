6886fd9c8e294153e69143438d60cb4c709e23d8
modulitos <luke@lukeswart.net>
2018-06-03 16:38:05 -0700

    fix(load_packages) autocomplete bug

--- load_packages.el
+++ load_packages.el
@@ -36,17 +36,10 @@
 
 ;; (add-to-list 'load-path "~/path-to/auto-complete")
 ; Load the default configuration
-(require 'auto-complete-config)
 
 ; Make sure we can find the dictionaries
 ;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete/dict")
 ; Use dictionaries by default
-(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
-(global-auto-complete-mode t)
-; Start auto-completion after 2 characters of a word
-(setq ac-auto-start 2)
-; case sensitivity is important when finding matches
-(setq ac-ignore-case nil)
 
 (require 'prettier-js)
 
