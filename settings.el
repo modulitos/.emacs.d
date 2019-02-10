cb46510055ae6e56d79ed1f2969a8050f352c19b
jalmogo <you@example.com>
2019-02-10 13:55:03 -0800

    use markdown-mode as default

--- settings.el
+++ settings.el
@@ -227,7 +227,7 @@
 ;; (global-visual-line-mode)
 ;; (toggle-truncate-lines)
 ;; (setq-default truncate-lines t)
-(setq-default major-mode 'org-mode)
+(setq-default major-mode 'markdown-mode)
 
 (setq-default global-visual-line-mode t)
 (setq visual-line-mode t)
