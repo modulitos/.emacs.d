89343f4e46fcd7b9603515aee58547c0259e1194
modulitos <you@example.com>
2020-01-12 19:02:45 -0800

    ruby rubocop

--- elisp/init-ruby.el
+++ elisp/init-ruby.el
@@ -5,7 +5,11 @@
 ;;; Code:
 (defun my-ruby-mode-hook ()
   (my-code-editor-hook)
-  (format-all-mode))
+  (format-all-mode)
+  ;; TODO: use prettier-ruby instead, ideally via format-all-mode
+  ;; TODO: pass this as a function hook to 'my-code-editor-hook'
+  (local-set-key (kbd "C-S-i") 'rubocopfmt)
+  )
 
 (add-hook 'ruby-mode-hook  'my-ruby-mode-hook)
 
