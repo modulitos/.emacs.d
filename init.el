de01103cbc96d8096683c3792e3937bb6b4b9fa4
modulitos <modulitos@users.noreply.github.com>
2020-11-22 14:54:51 -0800

    formatting

--- init.el
+++ init.el
@@ -75,16 +75,15 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(coffee-args-compile (quote ("-c" "--no-header" "--bare")))
+ '(coffee-args-compile '("-c" "--no-header" "--bare"))
  '(coffee-indent-like-python-mode t)
  '(coffee-tab-width 2)
- '(company-search-regexp-function (quote company-search-flex-regexp))
+ '(company-search-regexp-function 'company-search-flex-regexp)
  '(css-indent-offset 2)
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes '(zenburn))
  '(custom-safe-themes
-   (quote
-    ("3c9d994e18db86ae397d077b6324bfdc445ecc7dc81bb9d528cd9bba08c1dac1" default)))
- '(emojify-program-contexts (quote (comments)))
+   '("3c9d994e18db86ae397d077b6324bfdc445ecc7dc81bb9d528cd9bba08c1dac1" default))
+ '(emojify-program-contexts '(comments))
  '(erc-prompt-for-password nil)
  '(fci-rule-color "#383838")
  '(js-indent-level 2)
@@ -97,36 +96,31 @@
  '(mu4e-view-prefer-html t)
  '(mu4e-view-show-images t)
  '(org-agenda-files
-   (quote
-    ("~/Documents/mgmt-docs/calendar.trello" "~/Documents/mgmt-docs/floortek-calendar.trello")))
- '(org-agenda-log-mode-items (quote (closed clock state)))
+   '("~/Documents/mgmt-docs/calendar.trello" "~/Documents/mgmt-docs/floortek-calendar.trello"))
+ '(org-agenda-log-mode-items '(closed clock state))
  '(org-export-date-timestamp-format "%Y-%m-%d")
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
- '(org-trello-files (quote ("~/Documents/mgmt-docs/haxgeo-trello.trello")) nil (org-trello))
+ '(org-trello-files '("~/Documents/mgmt-docs/haxgeo-trello.trello") nil (org-trello))
  '(package-selected-packages
-   (quote
-    (rubocopfmt rubocop json-mode markdown-mode flycheck-mypy format-all iedit flymake-json use-package racer flycheck-rust rust-mode emojify prettier-js add-node-modules-path evil company-web tide pdf-tools git-gutter evil-magit magit yaml-mode web-mode w3m tss timesheet tern-auto-complete swiper php-mode paredit org-trello markdown-mode+ js2-refactor js-comint ix image+ htmlize helm-fuzzy-find helm-fuzzier helm-flyspell helm-dired-recent-dirs haskell-mode haml-mode flycheck exec-path-from-shell evil-smartparens evil-org erc-hl-nicks elpy dumb-jump django-snippets dired+ coffee-mode bookmark+ ac-js2)))
+   '(flymake-shellcheck rubocopfmt rubocop json-mode markdown-mode flycheck-mypy format-all iedit flymake-json use-package racer flycheck-rust rust-mode emojify prettier-js add-node-modules-path evil company-web tide pdf-tools git-gutter evil-magit magit yaml-mode web-mode w3m tss timesheet tern-auto-complete swiper php-mode paredit org-trello markdown-mode+ js2-refactor js-comint ix image+ htmlize helm-fuzzy-find helm-fuzzier helm-flyspell helm-dired-recent-dirs haskell-mode haml-mode flycheck exec-path-from-shell evil-smartparens evil-org erc-hl-nicks elpy dumb-jump django-snippets dired+ coffee-mode bookmark+ ac-js2))
  '(python-indent-guess-indent-offset nil)
  '(safe-local-variable-values
-   (quote
-    ((encoding . utf-8)
+   '((encoding . utf-8)
      (js2-basic-offset . 4)
      (js2-basic-offset . 2)
-     (truncate-lines . 1))))
+     (truncate-lines . 1)))
  '(sh-basic-offset 2)
  '(sh-indentation 2)
  '(show-trailing-whitespace t)
  '(timesheet-invoice-number 135)
  '(tls-program
-   (quote
-    ("gnutls-cli --insecure -p %p %h" "gnutls-cli --insecure -p %p %h --protocols ssl3" "openssl s_client -connect %h:%p -no_ssl2 -ign_eof" "openssl s_client -connect %h:%p -no_ssl2 -ign_eof -CAfile ~/.ssl/spi_ca.pem -cert ~/.ssl/nick.pem " "gnutls-cli --priority SECURE256 --x509cafile ~/.ssl/spi_ca.pem --x509certfile ~/.ssl/nick.pem -p %p %h")))
+   '("gnutls-cli --insecure -p %p %h" "gnutls-cli --insecure -p %p %h --protocols ssl3" "openssl s_client -connect %h:%p -no_ssl2 -ign_eof" "openssl s_client -connect %h:%p -no_ssl2 -ign_eof -CAfile ~/.ssl/spi_ca.pem -cert ~/.ssl/nick.pem " "gnutls-cli --priority SECURE256 --x509cafile ~/.ssl/spi_ca.pem --x509certfile ~/.ssl/nick.pem -p %p %h"))
  '(typescript-auto-indent-flag t)
  '(typescript-expr-indent-offset 0)
  '(typescript-indent-level 2)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
+   '((20 . "#BC8383")
      (40 . "#CC9393")
      (60 . "#DFAF8F")
      (80 . "#D0BF8F")
@@ -143,7 +137,7 @@
      (300 . "#7CB8BB")
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
+     (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3")
  '(w3m-default-display-inline-images t)
  '(web-mode-attr-indent-offset 2)
@@ -155,8 +149,8 @@
  '(web-mode-enable-comment-keywords nil t nil "TODO")
  '(web-mode-enable-control-block-indentation nil)
  '(web-mode-markup-indent-offset 2)
- '(whitespace-style (quote (trailing)))
- '(znc-erc-ssl-connector (quote erc-tls)))
+ '(whitespace-style '(trailing))
+ '(znc-erc-ssl-connector 'erc-tls))
 
 ;;; init.el ends here
 (custom-set-faces
