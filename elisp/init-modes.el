465c37373bf20e101022763ee79ca299f991cecb
modulitos <modulitos@users.noreply.github.com>
2021-02-06 20:19:38 -0800

    update evil mode

--- elisp/init-modes.el
+++ elisp/init-modes.el
@@ -3,7 +3,6 @@
 ;;; configs for various modes, dumping ground
 ;;; TODO: break this out into separate files
 
-(require 'evil)
 ;; functions shared across modes:
 (defun my-code-editor-hook ()
   "Hooks for all modes."
@@ -20,12 +19,6 @@
   (format-all-mode t))
 
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-config)
-
-
-;;; Code:
-;; INFO MODE
-(message "loading init-modes.el")
-(evil-set-initial-state 'Info-mode 'emacs)
 
 
 ;; HELM mode
@@ -281,10 +274,31 @@
 
 
 ;; Enable Evil mode as defuault
-(evil-mode 1)
+;; (require 'evil)
+;; (evil-mode 1)
 
-(global-undo-tree-mode -1)
-(evil-set-undo-system 'undo-fu)
+;; (global-undo-tree-mode -1)
+;; (evil-set-undo-system 'undo-fu)
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  (global-undo-tree-mode -1)
+  (evil-set-undo-system 'undo-fu)
+  ;; This prevents us from typing 'j', so using the cofi solution below:
+  ;; (define-key evil-insert-state-map "jj" 'evil-normal-state)
+  (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+  (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+
+  (define-key evil-normal-state-map [escape] 'keyboard-quit)
+  (define-key evil-visual-state-map [escape] 'keyboard-quit)
+  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+  )
 
 (use-package undo-fu
   :config
@@ -292,10 +306,34 @@
   (define-key evil-normal-state-map "u" 'undo-fu-only-undo)
   (define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo))
 
-(add-hook 'evil-mode-hook
-          (lambda()
-            (local-unset-key "C-/")
-            (local-unset-key "C-z")))
+
+;; Enable smash escape
+;; Type 'jj' for smash escape
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit-jj)
+(evil-define-command cofi/maybe-exit-jj ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "j")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
+                           nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?j))
+        (delete-char -1)
+        (set-buffer-modified-p modified)
+        (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                                              (list evt))))))))
+
+;; (add-hook 'evil-mode-hook
+;;           (lambda()
+;;             (local-unset-key "C-/")
+;;             (local-unset-key "C-z")
+;;             (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+;;             (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+;;             )
+;;           )
 
 (eval-after-load "evil-maps"
   (dolist (map '(evil-motion-state-map
@@ -307,25 +345,46 @@
     )
   )
 
+;; Hooks to enabe/disable evil in other modes
+(add-hook 'term-mode-hook 'evil-emacs-state)
+(add-hook 'ansi-term-mode-hook 'evil-emacs-state)
+
+
+
 ;;    (define-key (eval map) (kbd "C-/") nil)))
 
+;; (add-hook 'undo-tree-mode (lambda () (local-unset-key "C-/")))
 
+;; (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
+;; (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+;; ;; (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up) ;; C-u interferes with org-mode bindings
+;; ;; (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+;; (define-key evil-insert-state-map (kbd "C-u")
+;;   (lambda ()
+;;     (interactive)
+;;     (evil-delete (point-at-bol) (point))))
 
 ;; evil-little-word bindings for camelCase:
 ;; (define-key evil-operator-state-map (kbd "b") 'evil-backward-little-word-begin)
 ;; (define-key evil-normal-state-map (kbd "w") 'subword-right)
 ;; (define-key evil-normal-state-map (kbd "b") 'subword-left)
 
+;; ;; key translations
+;; ;; ie: translate zh to C-h and zx to C-x
+;; (define-key evil-normal-state-map "z" nil)
+;; (define-key evil-motion-state-map "zu" 'universal-argument)
+;; (define-key key-translation-map (kbd "zh") (kbd "C-h"))
+;; (define-key key-translation-map (kbd "zx") (kbd "C-x"))
 
 ;;; esc quits pretty much anything (like pending prompts in the minibuffer)
 
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key evil-normal-state-map [escape] 'keyboard-quit)
+;; (define-key evil-visual-state-map [escape] 'keyboard-quit)
+;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
 ;; (define-key evil-insert-state-map "ㅏ" #'cofi/maybe-exit-ㅏㅓ)
 ;;(define-key evil-insert-state-map "j" #'cofi/maybe-exit-ㅏㅓ)
@@ -379,28 +438,10 @@
 ;;(define-key evil-insert-state-map "a" #'test-my-key)
 ;;(define-key evil-insert-state-map "ㅏ" #'test-my-key) ; Not working!
 
-;; Enable smash escape
-;; Type 'jj' for smash escape
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit-jj)
-(evil-define-command cofi/maybe-exit-jj ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "j")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
-                           nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?j))
-        (delete-char -1)
-        (set-buffer-modified-p modified)
-        (push 'escape unread-command-events))
-       (t (setq unread-command-events (append unread-command-events
-                                              (list evt))))))))
-
-;; Hooks to enabe/disable evil in other modes
-(add-hook 'term-mode-hook 'evil-emacs-state)
-(add-hook 'ansi-term-mode-hook 'evil-emacs-state)
+;;; Code:
+;; INFO MODE
+(message "loading init-modes.el")
+(evil-set-initial-state 'Info-mode 'emacs)
 
 
 ;; CALENDAR MODE
