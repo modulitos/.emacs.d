85a1b7cb075476197f856065bf8e5632725c9c75
modulitos <you@example.com>
2021-02-20 15:13:00 -0800

    linux keybindings

--- elisp/keybindings.el
+++ elisp/keybindings.el
@@ -44,6 +44,7 @@
 ;; BUFFERS
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "s-p") 'helm-buffers-list)
+(global-set-key (kbd "C-p") 'helm-buffers-list)
 (with-eval-after-load 'evil-maps
   (define-key evil-normal-state-map (kbd "C-p") nil))
                                         ; switch keybindings for "save buffer" and "isearch-forward"
