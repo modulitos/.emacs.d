270a098ec3a3a9de06ba2f730282b7322f6d562d
modulitos <you@example.com>
2021-04-10 14:17:50 -0700

    go-mode

---
+++ elisp/init-go.el
@@ -0,0 +1,11 @@
+;;; Package --- Summary
+;;; Commentary:
+;;; config for golang mode
+
+
+;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.go" . go-mode))
+
+(provide 'init-go)
+;;; init-go.el ends here
