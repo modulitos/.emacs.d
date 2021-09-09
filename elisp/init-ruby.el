36f72c2f1d2a1d70267180bfdd13cd6c4ebc1b9d
modulitos <modulitos@users.noreply.github.com>
2021-09-08 17:17:20 -0700

    ruby

--- elisp/init-ruby.el
+++ elisp/init-ruby.el
@@ -4,11 +4,14 @@
 
 ;;; Code:
 (defun my-ruby-mode-hook ()
+  "Ruby mode hook!"
   (my-code-editor-hook)
-  (format-all-mode)
-  ;; TODO: use prettier-ruby instead, ideally via format-all-mode
+  ;; This causes a "peculiar error"?:
+  ;; (format-all-mode)
+  (envrc-global-mode)
   ;; TODO: pass this as a function hook to 'my-code-editor-hook'
-  (local-set-key (kbd "C-S-i") 'rubocopfmt)
+  ;; (local-set-key (kbd "C-S-i") 'rubocopfmt)
+  (local-set-key (kbd "C-S-i") 'rubocop-autocorrect-current-file)
   )
 
 (add-hook 'ruby-mode-hook  'my-ruby-mode-hook)
