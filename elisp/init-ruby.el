ea846dd9546527a80ede7e407c55830d688097fa
modulitos <modulitos@users.noreply.github.com>
2022-04-04 23:20:29 -0700

    direnv ruby

--- elisp/init-ruby.el
+++ elisp/init-ruby.el
@@ -9,9 +9,17 @@
 (defun my-ruby-mode-hook ()
   "Ruby mode hook!"
   (my-code-editor-hook)
+  ;; (message "loaded!")
+
+  ;; Treat underscores as part of the word:
+  ;; https://emacs.stackexchange.com/a/27350/2676
+  (modify-syntax-entry ?_ "w")
+
+  ;; (envrc-global-mode) ;; using direnv-mode instead
+  ;; (direnv-mode)  ;; this slows down switching between buffers, and might not be needed?
+
   ;; This causes a "peculiar error"?:
   ;; (format-all-mode)
-  (envrc-global-mode)
   ;; TODO: pass this as a function hook to 'my-code-editor-hook'
   ;; (local-set-key (kbd "C-S-i") 'rubocopfmt)
   (local-set-key (kbd "C-S-i") 'rubocop-autocorrect-current-file)
