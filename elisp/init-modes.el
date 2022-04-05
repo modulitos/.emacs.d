b117801d4e9c7a65e683813ea3c8d60aa970d9f2
modulitos <modulitos@users.noreply.github.com>
2022-04-04 23:19:50 -0700

    enable direnv

--- elisp/init-modes.el
+++ elisp/init-modes.el
@@ -669,6 +669,13 @@
 ;; GIT-GUTTER MODE
 (require 'git-gutter)
 
+
+;; DIRENV ENVRC
+(use-package direnv)
+;; :config
+;; (direnv-mode))
+
+
 ;; ;; If you enable global minor mode
 (global-git-gutter-mode t)
 ;; (global-git-gutter-mode +1)
